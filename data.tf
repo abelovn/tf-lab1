ddcb2acd6795243893fc7440a6a7333f7f179f2d
abelovn <Alexandr_Belov1@epam.com>
2022-03-28 23:17:15 +0300

    uodate lab1

--- data.tf
+++ data.tf
@@ -1,35 +1,22 @@
 
 data "aws_vpc" "main_vpc" {
-     filter {
-        name   = "tag:Owner"
-        values = ["a.belov.n@gmail.com"]
-  }
+     default = true
 }
 
 data "aws_subnet_ids" "existing_subnets" {
-    filter {
-        name   = "tag:Owner"
-        values = ["a.belov.n@gmail.com"]
-
-    }
+   
     vpc_id = data.aws_vpc.main_vpc.id
 }
 
 data "aws_subnet" "subnet" {
-    filter {
-        name   = "tag:Owner"
-        values = ["a.belov.n@gmail.com"]
-    }    
+    
 
     for_each   = data.aws_subnet_ids.existing_subnets.ids
     id         = each.value    
 }
 
 data "aws_security_groups" "security_groups" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.main_vpc.id]
-  }
+  
 }
 
 data "aws_security_group" "security_group" {
